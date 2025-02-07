20d14dabc956f1be8e5ad1300b2c8a056da6dfb3
Mohammad Athar <atharkhan9695@gmail.com>
2025-02-07 12:27:17 +0530

    CORS cookies error resolved

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -48,8 +48,6 @@ export const login = async (req, res) => {
         const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1w'});
         res.cookie("jwt", token, {
             httpOnly: true,
-            secure: true,
-            sameSite: "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
